72ad8c6530938b5be24f1ec7c91df2b17cfda18d
Harkaran Singh Tandon <harkaran.tandon@zaggle.in>
2020-08-31 18:57:00 +0530

    This is  a demo frontend application done as part of a course on frontend development using React on Coursera

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
+  ,
   document.getElementById('root')
 );
 
